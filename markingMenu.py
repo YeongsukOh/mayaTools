869dadf64a7bcb1a78c2fd49f0184b4d9ab92f79
yeongsuk Oh <yeongsukoh87@gmail.com>
2023-04-29 17:47:28 -0700

    "first pass marking menu"

---
+++ markingMenu.py
@@ -0,0 +1,36 @@
+import maya.cmds as cmds
+import importlib as reload
+
+class MarkingMenu():
+    
+    NAME = 'YSTOOL'
+    def __init__(self):
+        self.launch_menu()
+
+    def launch_menu(self):
+
+        menu = cmds.popupMenu(self.NAME,
+                             markingMenu = True,
+                             ctrlModifier = True,
+                             altModifier = True,
+                             shiftModifier = False,
+                             button = 1,
+                             parent = "viewPanes",
+                             postMenuCommandOnce = True,
+                             postMenuCommand = self.menuItem,
+                             )
+
+    
+    def menuItem(self, menu, parent):
+        
+        cmds.menuItem( parent = menu, label = "A", radialPosition = "NW")
+        cmds.menuItem( parent = menu, label = "B",radialPosition = "W")
+        cmds.menuItem( parent = menu, label = "C",radialPosition = "SW") 
+        cmds.menuItem( parent = menu, label = "D",radialPosition = "S")
+        cmds.menuItem( parent = menu, label = "E",radialPosition = "SE")
+        cmds.menuItem( parent = menu, label = "F",radialPosition = "E")
+        cmds.menuItem( parent = menu, label = "G",radialPosition = "NE")
+        cmds.menuItem( parent = menu, label = "H",  radialPosition = "N")
+
+reload(MarkingMenu)
+test = MarkingMenu()
