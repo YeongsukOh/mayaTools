51e5f155c02ff954f71e96dad53cfdb67b1f32d8
yeongsuk Oh <yeongsukoh87@gmail.com>
2023-05-04 10:27:26 -0700

    modified external library

--- markingMenu.py
+++ markingMenu.py
@@ -1,15 +1,15 @@
 import maya.cmds as cmds
-import importlib as reload
+from importlib import reload
 
 class MarkingMenu():
-    
+
     NAME = 'YSTOOL'
+    
     def __init__(self):
         self.launch_menu()
 
     def launch_menu(self):
-
-        menu = cmds.popupMenu(self.NAME,
+        dag = cmds.popupMenu(self.NAME,
                              markingMenu = True,
                              ctrlModifier = True,
                              altModifier = True,
@@ -17,20 +17,16 @@ class MarkingMenu():
                              button = 1,
                              parent = "viewPanes",
                              postMenuCommandOnce = True,
-                             postMenuCommand = self.menuItem,
+                             postMenuCommand = self.menuItems,
                              )
 
     
-    def menuItem(self, menu, parent):
-        
-        cmds.menuItem( parent = menu, label = "A", radialPosition = "NW")
-        cmds.menuItem( parent = menu, label = "B",radialPosition = "W")
-        cmds.menuItem( parent = menu, label = "C",radialPosition = "SW") 
-        cmds.menuItem( parent = menu, label = "D",radialPosition = "S")
-        cmds.menuItem( parent = menu, label = "E",radialPosition = "SE")
-        cmds.menuItem( parent = menu, label = "F",radialPosition = "E")
-        cmds.menuItem( parent = menu, label = "G",radialPosition = "NE")
-        cmds.menuItem( parent = menu, label = "H",  radialPosition = "N")
-
-reload(MarkingMenu)
-test = MarkingMenu()
+    def menuItems(self, dag, parent):
+        cmds.menuItem( label = "A", parent = dag, radialPosition = "NW")
+        cmds.menuItem( label = "B", parent = dag, radialPosition = "W")
+        cmds.menuItem( label = "C", parent = dag, radialPosition = "SW") 
+        cmds.menuItem( label = "D", parent = dag, radialPosition = "S")
+        cmds.menuItem( label = "E", parent = dag, radialPosition = "SE")
+        cmds.menuItem( label = "F", parent = dag, radialPosition = "E")
+        cmds.menuItem( label = "G", parent = dag, radialPosition = "NE")
+        cmds.menuItem( label = "H", parent = dag, radialPosition = "N")
